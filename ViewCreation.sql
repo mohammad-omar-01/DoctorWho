6bcec0d408efc167ac84b638681a63f619ab40f7
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-06-11 20:20:29 +0300

    View Creation For All episodes

--- ViewCreation.sql
+++ ViewCreation.sql
@@ -1,13 +1,11 @@
 ----------------------------View Creation ---------------------------
-
-alter VIEW  viewEpisodes AS 
+Alter VIEW  viewEpisodes AS 
 	SELECT A.AuthorName ,D.DoctorName, E.Title , dbo.fnEnemies(EpisodeId) AS EnemyNames , dbo.fnCompanion(EpisodeId) AS CompainonNames
 
 	FROM tblEpisode E left outer JOIN tblAuthor A ON 
 	E.AuthorId=a.AuthorId 
 	left outer JOIN tblDoctor D ON 
 	E.DoctorID= D.DoctorId;
-
 -------------------------#Test VIEW-----------------------
 SELECT * FROM dbo.viewEpisodes WHERE DoctorName is Not Null;
 
